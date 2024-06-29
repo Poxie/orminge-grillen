ceef2f5247bdb5e072368074c769358658e5a0bc
Poxie <poxen12@hotmail.com>
2024-06-30 01:30:29 +0200

    Connect price with menu title

--- components/home/HomeMenuItem.tsx
+++ components/home/HomeMenuItem.tsx
@@ -4,17 +4,17 @@ export default function HomeMenuItem({ item: { name, description, price } }: {
     item: MenuItem;
 }) {
     return(
-        <div className="py-3 px-3 -mx-3 flex justify-between sm:items-center gap-4 hover:bg-tertiary rounded-md transition-colors">
-            <div className="grid">
+        <div className="py-3 px-3 -mx-3 grid hover:bg-tertiary rounded-md transition-colors">
+            <div className="flex justify-between">
                 <span className="text-lg font-medium">
                     {name}
                 </span>
-                <span className="text-muted">
-                    {description}
+                <span className="font-medium">
+                    {price}kr
                 </span>
             </div>
-            <span className="mt-0.5 sm:mt-0 font-medium">
-                {price}kr
+            <span className="text-muted">
+                {description}
             </span>
         </div>
     )
