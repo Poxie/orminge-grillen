d434c959dc170b01cff6cf11d08c977341985c30
Poxie <poxen12@hotmail.com>
2024-06-30 01:53:45 +0200

    Remove menu item hover effect on smaller devices

--- components/home/HomeMenuItem.tsx
+++ components/home/HomeMenuItem.tsx
@@ -4,7 +4,7 @@ export default function HomeMenuItem({ item: { name, description, price } }: {
     item: MenuItem;
 }) {
     return(
-        <div className="py-3 px-3 -mx-3 grid hover:bg-tertiary rounded-md transition-colors">
+        <div className="py-3 px-3 -mx-3 grid md:hover:bg-tertiary rounded-md transition-colors">
             <div className="flex justify-between">
                 <span className="text-lg font-medium">
                     {name}
